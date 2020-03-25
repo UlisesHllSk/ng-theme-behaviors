02e7e4c012dc6c0ee9aad6ab3643b62abbfb2fa6
UlisesHllSk <divinecurse_hs@hotmail.com>
2020-03-25 11:31:10 -0600

    CHANGE - Changes project's name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-theme-core!');
+    expect(page.getTitleText()).toEqual('Welcome to ng-common-behaviors!');
   });
 
   afterEach(async () => {
