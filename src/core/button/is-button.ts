f36a58a38baedd3e13e1cdb7ab97e399530ad070
UlisesHllSk <divinecurse_hs@hotmail.com>
2020-07-29 12:45:08 -0500

    FEAT - Button's behavior

---
+++ src/core/button/is-button.ts
@@ -0,0 +1,4 @@
+import { CanDisableInterface } from '../disable/can-disable';
+import { HasColorInterface } from '../color/has-color';
+
+export interface IsButton extends CanDisableInterface, HasColorInterface { }
