7a73d73d2c8166fceec13a5144d3d73dfeb96700
UlisesHllSk <divinecurse_hs@hotmail.com>
2020-08-12 14:49:27 -0500

    REFACTOR - Changed project's name

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to ng-common-behaviors!');
+    expect(page.getTitleText()).toEqual('Welcome to ng-theme-behaviors!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ng-common-behaviors'),
+      dir: require('path').join(__dirname, './coverage/ng-theme-behaviors'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
